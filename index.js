c5f7df7159aa60fe1adda70059b03bfba416bf6d
Anny27-pixel <jabintasmin102523@gmail.com>
2022-10-26 20:27:14 +0600

    app create , port set , get and listen response

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-<h2>hello from server side </h2>
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('courses api is running');
+});
+
+app.listen(port, () => {
+    console.log('courses running in port', port);
+})
