dbff53c7bc58c8295c0f5bb4a73673fb86af4065
Anny27-pixel <jabintasmin102523@gmail.com>
2022-10-26 21:18:32 +0600

    cors create and use as a function to access data outside

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
+
+const categories = require('./data/categories.json');
+
+
 
 app.get('/', (req, res) => {
     res.send('courses api is running');
 });
+
+app.get('/courses-categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('courses running in port', port);
